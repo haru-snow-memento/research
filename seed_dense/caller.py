e38fadbf2c482f7c4ca674bd82076abd67079cb9
haru-snow-memento <oda.haruyuki.44n@st.kyoto-u.ac.jp>
2018-07-12 22:46:36 +0900

    modify caller module

--- seed_dense/caller.py
+++ seed_dense/caller.py
@@ -1,6 +1,50 @@
 #!/usr/bin/env python3
 
 from math import exp
+from math import sqrt
+from math import log
+from math import pi as PI
+
+class DecExpCaller(object):
+    """
+    (N0 / do)*exp(-d/d0) 
+    parameter are N0 and d0
+    """
+    def __init__(self, d0, N0=1.0):
+        self.param_info = ["decexp",
+                           " d0 = " + str(d0),
+                           " N0 = " + str(N0)]
+
+        def caller(d):
+            return (N0/d0)*exp(-d/d0)
+        self.caller = caller
+
+    def __call__(self, d):
+        return self.caller(d)
+
+
+
+class LogNormalCaller(object):
+    """
+    C = N0/(sigma*d*math.sqrt(2*PI))
+    A = (ln(d)-ln(d0))**2
+    C * exp(-A/(2*(sigma**2))
+    """
+    def __init__(self, d0, sigma, N0=1):
+        self.param_info = ["lognormal",
+                           "d0 = " + str(d0),
+                           "sigma = " + str(sigma),
+                           "N0 = " + str(N0)]
+
+        def caller(d):
+            c0 = N0/(sigma*d*sqrt(2*PI))
+            ao = (log(d)-log(d0))**2
+            ans = c0 * exp(-ao/(2*(sigma**2)))
+            return ans
+        self.caller = caller
+
+    def __call__(self, d):
+        return self.caller(d)
 
 
 class GaussCaller(object):
@@ -12,19 +56,10 @@ class GaussCaller(object):
 
         def caller(x):
             return C * exp(-a*(x - b)**2)
-        self.__call__ = caller
+        self.caller = caller
 
-
-class DecExpCaller(object):
-    def __init__(self, a, C=1):
-        self.param_info = ["decexp",
-                           " a = " + str(a),
-                           " C = " + str(C)]
-
-        def caller(x):
-            return C*exp(-a*x)
-        self.__call__ = caller
-
+    def __call__(self, x):
+        return self.caller(x)
 
 class LorentzCaller(object):
     def __init__(self):
