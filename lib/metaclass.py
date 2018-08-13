60d76c5a9794ff85c6b9a240f5b3584e4d211b46
haru-snow-memento <oda.haruyuki.44n@st.kyoto-u.ac.jp>
2018-08-13 19:17:13 +0900

    add commit

---
+++ lib/metaclass.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+class MetaBase(type):
+    BASE_METHODS = set(["get_x", "set_y"])
+    def __new__(cls, name,bases, kwrgs):
+        print(name)
+        print(bases)
+        print(kwrgs)
+        if not cls.BASE_METHODS.issubset(kwrgs.keys()):
+            raise TypeError
+        return type(name, bases, kwrgs)
+
+
+class Child(metaclass=MetaBase):
+    def __init__(self):
+        pass
+
