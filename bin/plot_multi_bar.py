5e0444e372542dc40c876bdc3191de9c186fe903
haru-snow-memento <oda.haruyuki.44n@st.kyoto-u.ac.jp>
2018-08-23 10:46:45 +0900

    add plot_multi_bar.py

--- bin/plot_multi_bar.py
+++ bin/plot_multi_bar.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def multi_bar_plot(re_ar, target_ar, width=0.3):
-    if len(re_ar) != len(target_ar):
+def multi_bar_plot(target_ar, ref_ar, width=0.3):
+    if len(ref_ar) != len(target_ar):
         raise IndexError("you must set the same length.")
-    target_pos = np.arange(len(re_ar))
+    target_pos = np.arange(len(ref_ar))
     ref_pos = target_pos + width
     plt.bar(target_pos, target_ar, width=0.3,
             align="center", label="target")
-    plt.bar(ref_pos, ref_pos, width=0.3,
+    plt.bar(ref_pos, ref_ar, width=0.3,
             align="center", label="ref")
     plt.legend(loc=2)
 
@@ -30,7 +30,9 @@ if __name__ == "__main__":
     REF_FILE_AR = args.ref_file_ar
     SAVE_PNG = args.save_png
     plt.ion()
-    multi_bar_plot(TARGET_FILE_AR, REF_FILE_AR)
+    ref_ar = np.loadtxt(REF_FILE_AR)
+    target_ar = np.loadtxt(TARGET_FILE_AR)
+    multi_bar_plot(target_ar, ref_ar)
     if SAVE_PNG is not None:
         plt.savefig(SAVE_PNG)
     else:
